f15b81330abe0beda82932f0c0df6be706bd829a
Sem van der Hoeven <semmer99@gmail.com>
2020-05-27 23:19:36 +0200

    fix error

--- core/src/netwerkprog/game/client/game/characters/DevTest1.java
+++ core/src/netwerkprog/game/client/game/characters/DevTest1.java
@@ -1,10 +1,11 @@
 package netwerkprog.game.client.game.characters;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import netwerkprog.game.util.game.GameCharacter;
 import netwerkprog.game.util.game.Faction;
 
 public class DevTest1 extends GameCharacter {
     public DevTest1() {
-        super("DevTest1", Faction.HACKER, "core/assets/ScifiCritters4.PNG");
+        super("DevTest1", Faction.HACKER, new TextureRegion());
     }
 }
